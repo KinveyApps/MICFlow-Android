b60ca87d5f2b99b29121e6487c03db833fa8e093
amoothart <amooth@gmail.com>
2016-10-25 15:27:28 -0400

    clean up comments

--- src/main/java/com/kinvey/sample/micflow/MainActivity.java
+++ src/main/java/com/kinvey/sample/micflow/MainActivity.java
@@ -20,12 +20,12 @@ import com.kinvey.java.User;
 public class MainActivity extends ActionBarActivity {
 
     private Client kinveyClient;
-    private static final String APP_KEY = "kid_ZyrG_IFcGe";
-    private static final String APP_SECRET = "f554f49974d14962bf72814268c2a7ba";
-    private static final String redirectURI = "kinveyAuthDemo://";
+    private static final String APP_KEY = "kid_Wy7NMiwaTx";
+    private static final String APP_SECRET = "18e581bc9c7046a5b1b20ae838105126";
+    private static final String redirectURI = "training://";
 
-    private static final String USERNAME = "test";
-    private static final String PASSWORD = "test";
+    private static final String USERNAME = "custom";
+    private static final String PASSWORD = "1234";
 
 
 
@@ -98,7 +98,6 @@ public class MainActivity extends ActionBarActivity {
 
     private void loginWithLoginPage(){
         loading();
-//        kinveyClient.user().presentMICLoginActivity(redirectURI, new KinveyUserCallback(){
         kinveyClient.user().loginWithAuthorizationCodeLoginPage(redirectURI, new KinveyMICCallback() {
 
             @Override
@@ -113,7 +112,6 @@ public class MainActivity extends ActionBarActivity {
 
             @Override
             public void onReadyToRender(String myURLToRender) {
-                //Time to render the login page for the user!
                 //This renders the login page with the device's default browser
                 Uri uri = Uri.parse(myURLToRender);
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
@@ -130,21 +128,6 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onSuccess(User user) {
                 updateStatus();
-
-//                AsyncAppData<HeaderEntity> myEvents = kinveyClient.appData("headers", HeaderEntity.class);
-//                myEvents.get(new KinveyListCallback<HeaderEntity>() {
-//                    @Override
-//                    public void onSuccess(HeaderEntity[] result) {
-//                        Log.d("INFO", result[0].toString());
-//                        loginStatus.setText("received " + result[0].toString());
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Throwable error) {
-//                        loginStatus.setText("failed to fetchByFilterCriteria" + error);
-//                    }
-//                });
-
             }
 
             @Override
